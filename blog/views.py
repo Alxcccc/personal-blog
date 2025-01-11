c5497c129c63548ed40235ae99cc59ca44b31cb8
Alxc <alecandres799@gmail.com>
2025-01-11 10:33:51 -0500

    templates and authentic

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, redirect
+from django.views.generic.base import View
+from django.views.generic.edit import UpdateView, DeleteView
+from django.urls import reverse_lazy
 
-# Create your views here.
+from .models import Article
+
+class ArticleListView(View):
+    def get(self, request, *args, **kwargs):
+        articles = Article.objects.all()
+        context = {
+            'articles': articles,
+        }
+        return render(request, 'blog/article_list.html', context)
--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from .views import HomeView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
     path('', HomeView.as_view(), name="Home"),
+    
+    path('accounts/', include('django.contrib.auth.urls'), name="Accounts"),
+    
+    path('blog/', include('blog.urls', namespace='blog')),
 ]
