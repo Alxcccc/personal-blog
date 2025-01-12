2ae5a301a172de7df9e2245531b7cf7dcce8cbed
Alxc <alecandres799@gmail.com>
2025-01-12 07:35:04 -0500

    view admin

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import ArticleListView
+from .views import ArticleListView, ArticleDetailView, ArticleEditAdminView, ArticleCreateAdminView, ArticleDeleteAdminView
 
 app_name = 'blog'
 
 urlpatterns = [
     
-    path('', ArticleListView.as_view(), name="home"),
-
+    path('', ArticleListView.as_view(), name='home'),
+    path('article/<int:id_article>', ArticleDetailView.as_view(), name='detail'),
+    path('article/edit/<int:pk>', ArticleEditAdminView.as_view(), name='update'),
+    path('article/new', ArticleCreateAdminView.as_view(), name='create'),
+    path('article/delete/<int:pk>', ArticleDeleteAdminView.as_view(), name='delete')
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from django.views.generic.edit import UpdateView, DeleteView
 from django.urls import reverse_lazy
 
 from .models import Article
+from .forms import ArticleCreateForm
 
 class ArticleListView(View):
     def get(self, request, *args, **kwargs):
@@ -12,3 +13,52 @@ class ArticleListView(View):
             'articles': articles,
         }
         return render(request, 'blog/article_list.html', context)
+    
+class ArticleDetailView(View):
+    def get(self, request, id_article, *args, **kwargs):
+        article = get_object_or_404(Article, pk=id_article)
+        context = {
+            'article': article
+        }
+        return render(request, 'blog/article_detail.html', context)
+
+class ArticleCreateAdminView(View):
+    def get(self, request, *args, **kwargs):
+        form = ArticleCreateForm()
+        context = {
+            'form': form
+        }
+        return render(request, 'blog/article_create.html', context)
+    
+    def post(self, request, *args, **kwargs):
+        if request.method == 'POST':
+            form = ArticleCreateForm(request.POST)
+            if form.is_valid():
+                title = form.cleaned_data.get('title')
+                content = form.cleaned_data.get('content')
+                
+                p, created = Article.objects.get_or_create(title=title, defaults={'content': content})
+                p.save()
+                return redirect('blog:home')
+        context = {
+            
+        }
+        return render(request, 'blog/article_create.html', context)
+    
+class ArticleEditAdminView(UpdateView):
+    model = Article
+    fields = ['title', 'content']
+    template_name = 'blog/article_update.html'
+    
+    def get_success_url(self):
+        pk = self.kwargs['pk']
+        return reverse_lazy('blog:detail', kwargs={'pk':pk})
+    
+class ArticleDeleteAdminView(DeleteView):
+    model = Article
+    template_name='blog/article_delete.html'
+    def get_success_url(self):
+        return reverse_lazy('blog:home')
+    
+    
+
